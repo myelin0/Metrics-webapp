a40a24823f5bea23ef89c6ce9452e87cc0b7f511
myelin0 <emi.veisllari@gmail.com>
2022-07-01 15:04:49 +0200

    Styling app

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './Hero.css';
+import worldmap from '../img/worldmap.png';
 
 
 const Hero = () => {
   return (
     <header>
+    <img className="world" src={worldmap}></img>
     <h2 className ="title">The world</h2>
     </header>
   )
